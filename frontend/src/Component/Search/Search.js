f980bbdc9a1da2498e4fb792b88abaac3922d7f8
Alejandro Roman <romanibanez.alex@gmail.com>
2024-08-08 07:53:01 +0200

    Added scroll to Search

--- frontend/src/Component/Search/Search.js
+++ frontend/src/Component/Search/Search.js
@@ -2,18 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { BiSearchAlt } from 'react-icons/bi';
 import './Search.css';
 
-// Mock data for products
-{/*const mockProducts = [
-    { _id: '1', name: 'Apple', description: 'Fresh Red Apple', price: 1.99 },
-    { _id: '2', name: 'Banana', description: 'Ripe Yellow Banana', price: 0.99 },
-    { _id: '3', name: 'Cherry', description: 'Sweet Red Cherry', price: 2.99 },
-    { _id: '4', name: 'Date', description: 'Delicious Dates', price: 3.99 },
-    { _id: '5', name: 'Elderberry', description: 'Healthy Elderberry', price: 4.99 },
-    { _id: '6', name: 'Fig', description: 'Nutritious Fig', price: 2.49 },
-    { _id: '7', name: 'Grape', description: 'Juicy Purple Grapes', price: 2.99 },
-    { _id: '8', name: 'Honeydew', description: 'Refreshing Honeydew Melon', price: 3.49 },
-];*/}
-
 const Search = ({ products }) => {
     const [query, setQuery] = useState('');
     const [suggestions, setSuggestions] = useState([]);
