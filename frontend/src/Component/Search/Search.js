263eb302614d9e6ba2ccce98ec8ea52e018006e6
Alejandro Roman <romanibanez.alex@gmail.com>
2024-08-10 00:02:42 +0200

    Added search click product

--- frontend/src/Component/Search/Search.js
+++ frontend/src/Component/Search/Search.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { BiSearchAlt } from 'react-icons/bi';
 import './Search.css';
+import { useNavigate } from 'react-router-dom';
 
 const Search = ({ products }) => {
     const [query, setQuery] = useState('');
     const [suggestions, setSuggestions] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchSuggestions = () => {
@@ -25,6 +27,11 @@ const Search = ({ products }) => {
         return () => clearTimeout(delayDebounceFn);
     }, [query, products]);
 
+    const handleSuggestionClick = (productId) => {
+        navigate(`/product/${productId}`); // Navigate to the product page
+    };
+
+
     return (
         <div className='search-cont'>
             <BiSearchAlt className='icon' />
@@ -37,11 +44,15 @@ const Search = ({ products }) => {
             {suggestions.length > 0 && (
                 <div className='suggestions'>
                     {suggestions.map((suggestion) => (
-                        <div key={suggestion._id} className='suggestion-item'>
-                            <p><strong>{suggestion.name}</strong></p>
-                            <p>{suggestion.description}</p>
-                            <p>{suggestion.price.toFixed(2)}€</p>
-                        </div>
+                        <div 
+                        key={suggestion._id} 
+                        className='suggestion-item' 
+                        onClick={() => handleSuggestionClick(suggestion._id)}
+                    >
+                        <p><strong>{suggestion.name}</strong></p>
+                        <p>{suggestion.description}</p>
+                        <p>{suggestion.price.toFixed(2)}€</p>
+                    </div>
                     ))}
                 </div>
             )}
